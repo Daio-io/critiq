f0ec6f82df03d870645fab0b67a473737b4137f2
Daio-io <dai.williams@bbc.co.uk>
2016-02-01 23:18:42 +0000

    Add author and full review

--- api/lib/ios.scrapper.js
+++ api/lib/ios.scrapper.js
@@ -11,7 +11,7 @@ iOSScrapper.prototype.scrape = function(appId) {
 
   let url = this.baseUrl + appId;
 
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
 
     // TODO - working: tidy up everything
     this.httpClient.get(url).then(body => {
@@ -25,9 +25,17 @@ iOSScrapper.prototype.scrape = function(appId) {
 
         for (let i = 0; i < reviewCount; i++) {
           let review = customerReviews.find('.customerReviewTitle').get(i);
+          let content = customerReviews.find('.content')[i];
           let starRating = customerReviews.find('.rating')[i].attribs['aria-label'];
-          let stars = starRating ? starRating.replace(/(?=star)\w+/, '') : '';
-          let rev = {text: review.children[0].data, rating: stars};
+          let star = starRating ? starRating.replace(/(?=star)\w+/, '').trim() : '';
+          let authorData = customerReviews.find('.user-info')[i];
+          let author = authorData.children[0].data.replace('by', '');
+
+          let rev = {short: review.children[0].data.trim(),
+            full: content.children[0].data.trim(),
+            star: star,
+            author: author.trim() };
+
           results.push(rev);
         }
 
@@ -38,7 +46,7 @@ iOSScrapper.prototype.scrape = function(appId) {
       }
 
     })
-      .catch(err => {
+      .catch(() => {
         resolve({status: 'failed', message: 'Error requesting app reviews. AppID is invalid', reviews: [] });
       })
   });
