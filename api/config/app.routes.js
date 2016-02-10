3ddf178aec44fbc83e5dfeb0fa0c174427151249
Daio-io <dai.williams@bbc.co.uk>
2016-02-10 11:24:33 +0000

    Add slack route

--- api/config/app.routes.js
+++ api/config/app.routes.js
@@ -1,9 +1,11 @@
 'use struct';
 
 const iosRouter = require('../ios/ios.router');
+const slackRouter = require('../slack/slack.router');
 
 module.exports = function(app) {
 
-  app.use(iosRouter.routes())
+  app.use(iosRouter.routes());
+  app.use(slackRouter.routes());
 
 };
