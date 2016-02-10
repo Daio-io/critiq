639ebdd528bda52d8bb3d441225430ea69d7839c
Daio-io <dai.williams@bbc.co.uk>
2016-02-10 11:37:28 +0000

    Only provide last 5 reviews

--- api/slack/slack.handlers.js
+++ api/slack/slack.handlers.js
@@ -1,26 +1,19 @@
 'use strict';
 
-const ParamFilter = require('../lib/param.filter');
-
 exports.getReviews = function *() {
 
   let appId = this.query.text;
-  let version = this.query.version;
-  let stars = this.query.stars;
 
   let data = yield this.Scrapper.scrape(appId);
 
-  let paramFilter = new ParamFilter(data.reviews);
-  data.reviews = paramFilter.filter('version', version)
-    .filter('stars', stars)
-    .results();
+  let attachments = data.reviews.map(data => {
+    return {title: data.version + ': ' + data.short, text: data.full};
+  });
 
-  let d = {
-    "response_type": "in_channel",
-    "text": "Latest reviews for app: " + data.app,
-    "attachments": data.reviews
+  this.body = {
+    response_type: "in_channel",
+    text: "Latest reviews for app: " + data.app,
+    attachments: attachments.splice(0, 5)
   };
-
-  this.body = d;
 
 };
